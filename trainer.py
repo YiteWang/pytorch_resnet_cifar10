5635d2996298bf36585e49e938641e6df1b0426b
YiteWang <yitew2@illinois.edu>
2020-09-23 23:15:19 -0500

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -12,6 +12,8 @@ import torch.utils.data
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import resnet
+import sampler
+import snip
 
 model_names = sorted(name for name in resnet.__dict__
     if name.islower() and not name.startswith("__")
